b1e77b23606040b6c51b3d66b35894c4829e7369
hoods <102035056+hhoods@users.noreply.github.com>
2022-04-05 08:46:41 +0800

    initial commit

--- day03-program-state/in-class/script.js
+++ day03-program-state/in-class/script.js
@@ -1,41 +1,70 @@
+var rollRandomWord = function () {
+  var randomDecimal = Math.random() * 3;
+  var randomWord;
+  var randomInteger = Math.floor(randomDecimal); // 0, 1, 2
+
+  if (randomInteger == 0) {
+    randomWord = "banana";
+  } else if (randomInteger == 1) {
+    randomWord = "chisel";
+  } else if (randomInteger == 2) {
+    randomWord = "faucet";
+  }
+
+  return randomWord;
+};
+
+var score = 2;
+
 var secretWordBaseMain = function (input) {
-  // Complete the Base: Secret Word exercise below with secretWordBaseMain as the main function.
-  var myOutputValue = 'hello world';
+  var secretWord = rollRandomWord();
+  var myOutputValue;
+  if (secretWord == input) {
+    score = score - 1;
+    myOutputValue = "you got it correct!";
+  } else {
+    myOutputValue = "please try again";
+  }
+  // now check if player meets the criteria to win the game
+  if (score == 0) {
+    myOutputValue = "congrats you win";
+  }
+
   return myOutputValue;
 };
 
 var secretWordTwiceRowMain = function (input) {
   // Complete the Comfortable: Secret Word Twice in a Row exercise below with secretWordTwiceRowMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var secretWordXRowMain = function (input) {
   // Complete the Comfortable: Secret Word X in a Row exercise below with secretWordXRowMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var diceWithinMain = function (input) {
   // Complete the More Comfortable: Dice Within exercise below with diceWithinMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var diceWithin2DiceMain = function (input) {
   // Complete the More Comfortable: Dice Within with 2 Dice exercise below with diceWithin2DiceMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var dice4DMain = function (input) {
   // Complete the More Comfortable: Dice 4D exercise below with dice4DeMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var secretWordTwice2Main = function (input) {
   // Complete the More Comfortable: Secret Word Twice in a Row 2 exercise below with secretWordTwice2Main as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
--- project1-sps/script.js
+++ project1-sps/script.js
@@ -1,4 +1,78 @@
-var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+// Step 1 : Create mathematical equation for bot to generate a try
+  // Step 1a: Attach a randomNumber to scissors, paper or stone
+
+var botTry = function () {
+  var randomDecimal = Math.random() * 3;
+  var randomTry;
+  var randomNumber = Math.floor(randomDecimal); // scissors, paper, stone
+
+  if (randomNumber == 0) {
+    return "scissors";
+  } 
+  
+  if (randomNumber == 1) {
+     return "paper";
+  }
+  
+  if (randomNumber == 2) {
+    return "stone";
 };
+
+
+// Step 2: Create a functional logic to determine how player wins the game
+  // scissors beats paper not stone
+  // paper beats stone not scissors
+  // stone beats scissors not paper
+
+
+var playerToWin = function (playerTry, botTry) {
+  return (
+    (playerTry == scissors && botTry == paper) ||
+    (playerTry == paper && botTry == stone) ||
+    (playerTry == stone && botTry == scissors)
+  )
+};
+
+
+// Step 3: Create a functional logic to determine if it's a draw
+  // scissors = scissors
+  // paper = paper
+  // stone == stone
+
+var playerToDraw = function (playerTry, botTry) {
+  return (
+    (playerTry == scissors && botTry == scissors) ||
+    (playerTry == paper && botTry == paper) ||
+    (playerTry == stone && botTry == stone)
+  )
+};
+
+
+// Step 4: Create a functional logic to determine if player loses
+  // scissors < stone
+  // paper < scissors
+  // stone < paper
+
+var playerToLose = function (playerTry, botTry) {
+  return (
+    (playerTry == scissors && botTry == stone) ||
+    (playerTry == paper && botTry == scissors) ||
+    (playerTry == stone && botTry == paper)
+  )
+};
+
+
+// Step 5: Define messaging for various outcomes (to Step 2, 3 or 4)
+
+
+if (input == playerToWin) {
+  return "Congrats you WON!";
+};
+
+if (input == playerToDraw) {
+  return "It's a DRAW. Try again.";
+}; 
+
+if (input == playerToLose) {
+  return "Oh no, you LOST"
+}
